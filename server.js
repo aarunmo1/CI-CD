8b638985588122b3bea776010ddb12f504d7fe8c
Aravind <arunmo1@ford.com>
2021-07-22 17:56:05 +0530

    chanhe

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-var message = "Hackathon content -- 1"
+var message = "Hackathon content"
 
 app.get('/', (req, res)=>{
     res.send(message)
